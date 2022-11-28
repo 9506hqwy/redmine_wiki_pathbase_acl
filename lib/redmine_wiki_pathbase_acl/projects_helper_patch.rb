1ec83037cb7af238b422401abcc88614185d4e65
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2022-11-29 00:10:40 +0900

    Redmine3 で他のプラグインが ProjectsController を拡張した場合にプロジェクトの設定が表示できない問題を修正した。

--- lib/redmine_wiki_pathbase_acl/projects_helper_patch.rb
+++ lib/redmine_wiki_pathbase_acl/projects_helper_patch.rb
@@ -2,7 +2,7 @@
 
 module RedmineWikiPathbaseAcl
   module ProjectsHelperPatch
-    def wiki_pathbase_acl_setting_tabs(tabs)
+    def project_settings_tabs
       action = {
         name: 'wiki_pathbase_acl',
         controller: :wiki_pathbase_acl,
@@ -11,38 +11,13 @@ module RedmineWikiPathbaseAcl
         label: :wiki_pathbase_acl,
       }
 
+      tabs = super
       tabs << action if User.current.allowed_to?(action, @project)
       tabs
     end
   end
-
-  module ProjectsHelperPatch4
-    include ProjectsHelperPatch
-
-    def self.included(base)
-      base.class_eval do
-        alias_method_chain(:project_settings_tabs, :wiki_pathbase_acl)
-      end
-    end
-
-    def project_settings_tabs_with_wiki_pathbase_acl
-      wiki_pathbase_acl_setting_tabs(project_settings_tabs_without_wiki_pathbase_acl)
-    end
-  end
-
-  module ProjectsHelperPatch5
-    include ProjectsHelperPatch
-
-    def project_settings_tabs
-      wiki_pathbase_acl_setting_tabs(super)
-    end
-  end
 end
 
-if ActiveSupport::VERSION::MAJOR >= 5
-  Rails.application.config.after_initialize do
-    ProjectsController.send(:helper, RedmineWikiPathbaseAcl::ProjectsHelperPatch5)
-  end
-else
-  ProjectsHelper.include RedmineWikiPathbaseAcl::ProjectsHelperPatch4
+Rails.application.config.after_initialize do
+  ProjectsController.send(:helper, RedmineWikiPathbaseAcl::ProjectsHelperPatch)
 end
